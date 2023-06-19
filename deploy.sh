34f98092255250a502b263f796d4b55fadf6a082
gitjohnmercado <jhon.mercado1578@gmail.com>
2023-06-19 10:46:23 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:jhonklose/react-nick-and-morty-john-mercado
+git push -f git@github.com:jhonklose/react-nick-and-morty-john-mercado.git main:gh-pages
 
 cd -
