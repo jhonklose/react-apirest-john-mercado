001ea467127fe732bab21899221021f6a9defd7d
gitjohnmercado <jhon.mercado1578@gmail.com>
2023-06-19 10:34:54 -0500

    second commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"https://jhonklose.github.io/react-nick-and-morty-john-mercado",
+  base:"https://github.com/jhonklose/react-nick-and-morty-john-mercado",
   plugins: [react()],
 })
